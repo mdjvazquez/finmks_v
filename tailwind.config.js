b2676b5ed1f582367df819d159d67a674d0b4fc7
Manuel de Jesus <manuel.vazquez@warlup.com>
2025-11-23 11:54:08 -0600

    Fix: Repare 5 Tailwind to v3.4 for stability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/*.{html,tsx}",
+    "./components/*.{html,tsx}",
+    "./*.{html,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
