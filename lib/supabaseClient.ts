2232e8d7f11dd57dbd33d075009d1a21859ef9fc
Manuel de Jesus <manuel.vazquez@warlup.com>
2025-11-23 08:02:56 -0600

    env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://zozsbxivnpmmblypcbaq.supabase.co';
-const supabaseKey = 'sb_publishable_KmLOQ9CgrsM-gZX8vw-Ekg_XKthRVTQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
